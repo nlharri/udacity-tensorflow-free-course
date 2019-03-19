ac57ce1004832156f716112370043f307edb65a0
László Harri Németh <nlharri@gmail.com>
2019-03-19 04:18:48 +0100

    Install scripts

---
+++ CreateMachineLearningVirtualEnv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Prerequisites: Python3, Pip3, Virtualenv are already installed.
+virtualenv -p python3 ./MachineLearningVirtualEnv
---
+++ InstallMachineLearningAppsInVirtualEnv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Prerequisites: Virtualenv is started.
+# Packages installed by this script: tensorflow, jupyter, matplotlib, tensorflow_datasets
+pip install --upgrade tensorflow
+pip install --upgrade jupyter
+pip install --upgrade matplotlib
+pip install --upgrade tensorflow_datasets
